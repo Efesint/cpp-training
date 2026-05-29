96475de858a670639457a21685bbbae54fa57713
efesint <brajanmozer53@gmail.com>
2026-05-29 11:34:36 +0300

    1.cpp
    цикл for, все делители числа 24.

--- for/1.cpp
+++ for/1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+//все делители числа 24
+int main()
+{
+   for(int i = 1; i <= 24; ++i)
+    {
+
+        if(24 % i == 0)
+	  {
+
+             std::cout << i << " ";
+	  
+	  }
+
+    }
+
+
+return 0;
+}
 
