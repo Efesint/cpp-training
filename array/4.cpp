7ad0c3ec3b58390a3f672b62437ed5afaac56476
efesint <brajanmozer53@gmail.com>
2026-05-31 22:31:29 +0300

    4.cpp

---
+++ array/4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//сдвиг, двигаем число под индексом 5 в первый индекс.
+int main()
+{
+int arr[5] = {1, 2, 3, 4, 5};
+int last = arr[4];
+       for(int i = 3; i >= 0; --i)
+         {
+
+           arr[i+1] = arr[i];
+
+	 }
+
+        arr[0] = last;
+
+       for(int i = 0; i < 5; ++i)
+         {
+
+            std::cout << arr[i] << " ";
+
+	 }
+return 0;
+}
