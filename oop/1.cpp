77d308a1d3387c7f4b4f47ab86f0bed6bd7568c7
efesint <brajanmozer53@gmail.com>
2026-06-03 00:21:48 +0300

    Create 1.cpp

---
+++ oop/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//сумма двух чисел;
+  int sum(int a, int b)
+    {
+        return a+b;
+    }
+
+
+
+int main()
+{
+
+int result = sum(7,13);
+std::cout << result;
+
+
+return 0;
+}
