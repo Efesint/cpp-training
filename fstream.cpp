9445d85a6551eecac256418ed7a86f8ae8b99e67
efesint <brajanmozer53@gmail.com>
2026-06-10 23:18:47 +0300

    Create fstream.cpp

---
+++ fstream.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+   std::ifstream file("1.txt");
+   std::string line;
+   while(std::getline(file, line)) {
+	   std::cout << line << std::endl;
+   }
+
+   file.close();
+   
+
+return 0;
+}
