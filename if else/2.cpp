08740e84fc86417fd9fabaf0f2d49ca446d8db06
efesint <brajanmozer53@gmail.com>
2026-06-10 22:52:29 +0300

    Create 2.cpp

---
+++ if else/2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    if( x > 0)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "no";
+    }
+}
