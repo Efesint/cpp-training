3faeda81be08e511f6c11b3b3a44cc67f6bcc6dc
efesint <brajanmozer53@gmail.com>
2026-06-17 21:38:58 +0300

    Create front()back().cpp

---
+++ vector/front()back().cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+std::vector<int> v = {10, 20, 30, 40, 50};
+std::cout << v.front() << std::endl;
+std::cout << v.back() << std::endl;
+
+return 0;
+}
+
