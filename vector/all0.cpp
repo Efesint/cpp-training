8a1e754e093459103f76611ec6fb8ea8ad12538d
efesint <brajanmozer53@gmail.com>
2026-06-17 21:27:02 +0300

    Update all0.cpp

--- vector/all0.cpp
+++ vector/all0.cpp
@@ -12,4 +12,18 @@ int main()
 
 return 0;
 }
+//#include <vector>
+//#include <iostream>
+
+//int main()
+//{
+//	std::vector<int> v(5, 42);
+
+//	for(int i = 0; i < v.size(); ++i)
+//	{
+//          std::cout << v[i] << " ";
+//	}
+
+//return 0;
+//}
 
