ab1b7354e50d130d288537c491a0c77a1d0d13da
efesint <brajanmozer53@gmail.com>
2026-06-17 21:23:43 +0300

    Create all0.cpp

---
+++ vector/all0.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v(5);
+	for(int i = 0; i < v.size(); ++i)
+	{
+          std::cout << v[i] << " ";
+	}
+
+
+return 0;
+}
+
