fc70873cfcdda9abb167061e4fab57e00ae0118a
efesint <brajanmozer53@gmail.com>
2026-06-18 17:38:20 +0300

    Create front()back().cpp

---
+++ string/front()back().cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+std::string s = "programming";
+std::cout << s.front() << std::endl;
+std::cout << s.back() << std::endl;
+
+
+return 0;
+}
+
