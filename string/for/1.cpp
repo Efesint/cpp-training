3e0dbbe0a5cf52fad92c3f31d28d3d10c6d09ac6
efesint <brajanmozer53@gmail.com>
2026-06-18 19:36:27 +0300

    Create 1.cpp

---
+++ string/for/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+std::string s = "Hello";
+for(int i = 0; i < s.size(); ++i)
+{
+   std::cout << s[i] << std::endl;
+}
+
+
+return 0;
+}
+
+~/
