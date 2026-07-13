b5d6a552af769c140d7ae2933139a8deaf8c49fe
efesint <brajanmozer53@gmail.com>
2026-07-13 15:54:48 +0300

    Create 8.cpp

---
+++ pointers/8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  // статический массив
+  int a[4] = {5, 7, 0, 4};
+  // размер масива в байтах
+  std::cout << sizeof(a);
+
+  std::cout << std::endl;
+
+  
+}
